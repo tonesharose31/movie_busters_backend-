9f101d5f005a38bd0181bc2eb9c3d2b11497b26a
Genesis Lara <genesislara@pursuit.org>
2023-11-06 21:43:44 -0500

    completes app file

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const moviesController = require("./controllers/moviesController");
+const reviewsController = require("./controllers/reviewsController");
+
+app.use(cors());
+app.use(express.json());
+app.use("/movies", moviesController);
+
+app.get("/", (req,res) => {
+  res.send("Welcome to Movie Busters! With Postgres");
+}
+);
+
+app.get("*", (req,res) => {
+  res.status(404).json({ success: false, data: {error: "page not found"}});
+});
+
+module.exports = app;
