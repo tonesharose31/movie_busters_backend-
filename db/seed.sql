d928c59df85bad2fbc459a40610b20c86abad4cf
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-11-07 20:30:44 -0500

    created data to seed to the table

---
+++ db/seed.sql
@@ -0,0 +1,9 @@
+\c movies_dev;
+
+INSERT INTO movies (title, year_of_release, genres, description, rating, runtime)
+VALUES
+  ('The Shawshank Redemption', 1994, '{Drama}', 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', 9.3, 142),
+  ('Inception', 2010, '{Action, Sci-Fi}', 'A thief who enters the dreams of others to steal their secrets.', 8.8, 148),
+('Forrest Gump', 1994, '{ Drama, Romance}', 'The life journey of a man with low intelligence who rose to become a football legend.', 8.8, 142);
+
+
