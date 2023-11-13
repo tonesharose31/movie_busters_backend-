3512206c48e19f53391e78f5dcb52e6fefba175c
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-11-13 04:57:18 -0500

    created an additional table for user

--- db/schema.sql
+++ db/schema.sql
@@ -4,10 +4,29 @@ CREATE DATABASE movies_dev;
 
 \c movies_dev;
 
-CREATE TABLE movies ( id SERIAL PRIMARY KEY, title TEXT NOT NULL, year_of_release INTEGER, genres TEXT[], description TEXT, rating TEXT, runtime INTEGER );
 
-CREATE TABLE reviews ( id SERIAL PRIMARY KEY, movie_id INTEGER REFERENCES movies(id)
-ON
-DELETE CASCADE, user_id INTEGER REFERENCES users(id)
-ON
-DELETE CASCADE, rating INTEGER, comment TEXT, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP );
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE movies ( id SERIAL PRIMARY KEY, 
+title TEXT NOT NULL, 
+year_of_release INTEGER, 
+genres TEXT[], 
+description TEXT, 
+rating TEXT, 
+runtime INTEGER );
+
+
+CREATE TABLE reviews ( id SERIAL PRIMARY KEY, 
+movie_id INTEGER REFERENCES movies(id)ON DELETE CASCADE, 
+user_id INTEGER REFERENCES users(id)ON DELETE CASCADE, 
+rating INTEGER, 
+comment TEXT, 
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
+);
