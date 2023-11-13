5efb21040ed2ad038b35f90dede3b8541e211f1b
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-11-13 06:08:17 -0500

    update

--- db/seedScript.js
+++ db/seedScript.js
@@ -1,15 +1,20 @@
+const fs = require('fs');
 const { insertUserWithHashedPassword } = require('../queries/users');
 
-const bcrypt = require('bcrypt');
 
 // Hash passwords using bcrypt
 const password1 = 'password_for_user1';
 const password2 = 'password_for_user2';
 
-bcrypt.hash(password1, 10, function(err, hash1) {
-  bcrypt.hash(password2, 10, function(err, hash2) {
+// Create user insertion queries with hashed passwords
+async function generateQueries() {
+    try {
+const userQuery1 = insertUserWithHashedPassword('user1', 'user1@example.com', password1);
+const userQuery2 = insertUserWithHashedPassword('user2', 'user2@example.com', password2);
 
-    console.log(`INSERT INTO users (username, email, password_hash) VALUES ('user1', 'user1@example.com', '${hash1}');`);
-    console.log(`INSERT INTO users (username, email, password_hash) VALUES ('user2', 'user2@example.com', '${hash2}');`);
-  });
-});
+fs.writeFileSync('db/seed.sql', `${userQuery1}\n${userQuery2}\n`, { flag: 'w' });
+} catch (error) {
+    console.error('Error generating queries:', error);
+  }
+}
+generateQueries();
--- queries/users.js
+++ queries/users.js
@@ -1,3 +1,4 @@
+// src/queries/userQueries.js
 const bcrypt = require('bcrypt');
 
 // Function to hash a password using bcrypt
@@ -5,13 +6,20 @@ const hashPassword = async (password) => {
   return bcrypt.hash(password, 10);
 };
 
-// Function to insert a user with a hashed password
-const insertUserWithHashedPassword = async (username, email, password) => {
-  const hashedPassword = await hashPassword(password);
 
-  return `INSERT INTO users (username, email, password_hash) VALUES ('${username}', '${email}', '${hashedPassword}');`;
+const insertUserWithHashedPassword = async (username, email, password) => {
+  try {
+    const hashedPassword = await hashPassword(password);
+
+  
+    return `INSERT INTO users (username, email, password_hash) VALUES ('${username}', '${email}', '${hashedPassword}');`;
+  } catch (error) {
+    console.error('Error hashing password:', error);
+    throw error;
+  }
 };
 
 module.exports = {
   insertUserWithHashedPassword,
 };
+
