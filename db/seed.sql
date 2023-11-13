51ac88314c07a58f085c3be5a7f6f065182d8b5b
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-11-13 05:23:33 -0500

    downloaded bcrypt for secure hashing algorithim

--- db/seed.sql
+++ db/seed.sql
@@ -1,5 +1,10 @@
 \c movies_dev;
 
+INSERT INTO users (username, email, password_hash)
+VALUES
+  ('user1', 'user1@example.com', 'hashed_password1'),
+  ('user2', 'user2@example.com', 'hashed_password2'),
+
 INSERT INTO movies (title, year_of_release, genres, description, rating, runtime)
 VALUES
 ('The Shawshank Redemption', 1994, '{Drama}', 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', 9.3, 142),
@@ -8,10 +13,10 @@ VALUES
 ('The Matrix',1999, '{Action, Sci-Fi}', 'A computer hacker learns about the true nature of his reality and his role in the war against its controllers.', 8.7,136), 
 ('The Conjuring', 2013, '{Horror, Paranormal}', 'Research uncovers a harrowing history of the house, linking it to accused witch Bathsheba Sherman, who, in 1863, sacrificed her infant to the devil, committed suicide, and left a legacy of murders and suicides on the property.', 7.5, 112);
 
-INSERT INTO reviews (movie_id, reviewer, rating, content, title) 
+INSERT INTO reviews (movie_id, user_id, rating, comment) 
 VALUES (1, 1, 5, 'Absolutely loved it! The storyline and acting were outstanding.'),
   (1, 2, 4, 'A classic film with a powerful message. Enjoyed every moment.'),
   (2, 1, 4, 'Mind-blowing! The concept of dreams within dreams was fascinating.'),
-  (2, 3, 5, 'Inception is a masterpiece. Christopher Nolan\'s brilliance shines through.');
+  (2, 3, 5, 'Inception is a masterpiece. Christopher Nolan\s brilliance shines through.');
 
 
